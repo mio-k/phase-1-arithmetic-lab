f7114a1912bd0178ebb4b6763272a21612e848f4
mio kataoka <mio.kanna@gmail.com>
2022-02-22 20:35:12 -0800

    commit tree f04f9bcb718c7e95f4b3f4ff35a91970df101334

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-function multiply(num1, num2) {
-    num1 * num2 === 62;
-}
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
 
-//let multiply;
-//let num1 = number
-//let num2 = number
-//multiply = num1 * num2 === 62;
+let random = Math.floor(Math.random()* 10) +1;
+
+let num3 = 32;
+let num4 = 7;
+let mod = num3 % num4;
+
+let max;
+max = Math.max(1,2,3,4,5,20);
